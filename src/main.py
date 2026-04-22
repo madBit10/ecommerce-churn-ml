e2c289e19a76365048f860aa420c267353193f62
madBit10 <vrushankraval48617@gmail.com>
2026-04-21 22:31:18 -0400

    initial commit pushing everything first

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# Basic entry point of a project (CLI starts here)
+
+# The main.py job is to do 3 things
+# 1. Listen - What did the user type? (--train? --predict?)
+# 2. Decide - okay, they want to train, let me call the traning pipeline
+# 3. Hand off - it calls the right pipline and let it do the actual work
+
+# Front door of the application 
+
---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,13 @@
+# It orchestrates ("manages") the entire traning process
+
+"""
+calls:
+
+step1 - load_data.py "Gets the raw data"
+step2 - preprocess.py "Clean and prepare it"
+step3 - train.py "Train the model"
+step4 - evaluate.py "Check how good it is"
+step5 - save_artifacts.py "Save the model and results"
+
+"""
+
